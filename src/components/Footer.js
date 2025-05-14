c846e7b9c02022c45dd8bd81ecad8d362444da85
Kim Hanz <nanaka3@hotmail.com>
2025-05-14 15:51:59 +0700

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import { socialLinks } from '../data.js';
+import PageLinks from './PageLinks.js';
+import SocialLink from './SocialLink.js';
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />;
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"></span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/Services.js
@@ -0,0 +1,17 @@
+import Title from './Title.js';
+import Service from './Service.js';
+import { services } from '../data.js';
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+      <div className="section-center services-center">
+        {services.map((service) => {
+          return <Service key={service.id} {...service} />;
+        })}
+      </div>
+    </section>
+  );
+};
+export default Services;
